a479babc04714e1853f542943af45884d88d9663
Basim Al-Jawahery <basimdev@gmail.com>
2025-05-24 16:43:27 +1200

    Tech/issue 11 setup jest unit testing (#12)
    * refact(data)!: move rules and schemas to the data folder, so the data folder contains of all data-related files;
    * feat(tests): set up jest infrastructure;
    * feat(tests): add tests to validate schemas;
    * feat(tests): add tests to validate the uniqueness of rule Ids and titles
    * tech(ci): add job to run unit tests in CI.
    * refact(data)!: remove categories and use just an array of rules;
    
    ---------
    
    Co-authored-by: Basim Al-Jawahery <basimdev@gmail.com>

---
+++ tests/__tests__/data-uniqueness.test.js
@@ -0,0 +1,90 @@
+const fs = require('fs-extra');
+const glob = require('glob');
+const path = require('path');
+
+const RULE_FILE_PATTERN = '**/*.rules.json';
+const RULES_FOLDER_PATH = path.resolve(__dirname, '../../data/rules/');
+
+describe('Validate data uniqueness', () => {
+
+    test('should not have duplicated rule ids', () => {
+        // Arrange
+        const ruleFiles = glob.sync(RULE_FILE_PATTERN, {cwd: RULES_FOLDER_PATH});
+        const rules = new Map();
+        const duplicatedKeys = new Set();
+
+        // Act
+        ruleFiles.forEach(file => {
+            const fileFullName = path.resolve(RULES_FOLDER_PATH, file);
+            const arrayOfRules = JSON.parse(fs.readFileSync(fileFullName, 'utf8'));
+
+            findDuplicatedRules({
+                keyAccessor: x => x.ruleId,
+                arrayOfRules,
+                ruleIds: rules,
+                duplicatedKeys,
+                fileName: fileFullName
+            });
+        })
+
+        // Assert
+        let errorMsg = '';
+        if (duplicatedKeys.size > 0) {
+            const firstDuplicateId = duplicatedKeys.values().next().value;
+            const otherFiles = rules.get(firstDuplicateId).map(info => info.fileName).join('\n');
+            errorMsg = `RuleId: ${firstDuplicateId} is duplicated in the following files:\n${otherFiles}`;
+        }
+        expect(duplicatedKeys.size, errorMsg).toBe(0);
+    });
+
+    test('should not have duplicated rule titles', () => {
+        // Arrange
+        const ruleFiles = glob.sync(RULE_FILE_PATTERN, {cwd: RULES_FOLDER_PATH});
+        const rules = new Map();
+        const duplicatedKeys = new Set();
+
+        // Act
+        ruleFiles.forEach(file => {
+            const fileFullName = path.resolve(RULES_FOLDER_PATH, file);
+            const arrayOfRules = JSON.parse(fs.readFileSync(fileFullName, 'utf8'));
+
+            findDuplicatedRules({
+                keyAccessor: x => x.title,
+                arrayOfRules,
+                ruleIds: rules,
+                duplicatedKeys,
+                fileName: fileFullName
+            });
+        })
+
+        // Assert
+        let errorMsg = '';
+        if (duplicatedKeys.size > 0) {
+            const firstDuplicateId = duplicatedKeys.values().next().value;
+            const otherFiles = rules.get(firstDuplicateId).map(info => info.fileName).join('\n');
+            errorMsg = `Rule title: "${firstDuplicateId}" is duplicated in the following files:\n${otherFiles}`;
+        }
+        expect(duplicatedKeys.size, errorMsg).toBe(0);
+    });
+
+    function findDuplicatedRules(info) {
+        const {keyAccessor, arrayOfRules, ruleIds, duplicatedKeys, fileName} = info;
+        arrayOfRules.forEach(rule => {
+            const key = keyAccessor(rule);
+            const existedInfo = ruleIds.get(key)
+            if (existedInfo === undefined) {
+                ruleIds.set(key, [{
+                    fileName,
+                    rule
+                }])
+            } else {
+                duplicatedKeys.add(key);
+                existedInfo.push({
+                    fileName,
+                    rule
+                })
+            }
+        })
+    }
+})
+;
